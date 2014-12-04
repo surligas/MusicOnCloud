12305b170d39a8f10f8560653078fb7785aadc70
Manos Surligas <surligas@csd.uoc.gr>
2014-12-04 15:32:35 +0200

    Some changes

--- media/mediaplayer.c
+++ media/mediaplayer.c
@@ -6,6 +6,8 @@
  */
 
 #include "mediaplayer.h"
+#include <error.h>
+#include <stdio.h>
 
 void
 init_mediaplayer(int argc, char *argv[])
@@ -17,3 +19,12 @@ void
 free_mediaplayer();
 
 
+size_t
+get_mp3_bit_rate(const char *filename)
+{
+	FILE *fp = fopen(filename, "r");
+	if(!fp){
+		perror("could not open file");
+		return 0;
+	}
+}
